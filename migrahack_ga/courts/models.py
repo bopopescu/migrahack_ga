993e1f3cb8b6ea50efc611a2d5c3d304420fb419
matt kiefer <matthewlkiefer@gmail.com>
2016-06-04 16:54:12 +0000

    starting courts app

---
+++ migrahack_ga/courts/models.py
@@ -0,0 +1,41 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+
+class Court(models.Model):
+    """
+    immigration courts
+    have judges, cases, plaintiffs
+    and defendants 
+    """
+    name     = models.CharField(max_length=50)
+    location = models.CharField(max_length=100)
+    state    = models.CharField(max_length=20)
+
+
+class Asylum(models.Model):
+    court      = models.ForeignKey(Court, null=True)
+    year       = models.IntegerField()
+    grants     = models.IntegerField()
+    denials    = models.IntegerField()
+    grant_rate = models.FloatField()
+
+
+class Case(models.Model):
+    case_no  = models.CharField(max_length=100)
+    court    = models.ForeignKey(Court, null=True)
+    judge    = models.ForeignKey(Judge, null=True)
+
+
+class Judge(models.Model):
+    name     = models.CharField(max_length=50)
+    court    = models.ForeignKey(Court, null=True)
+
+
+class Individual(models.Model):
+    name = models.CharField(max_length=50)
+    case = models.ForeignKey(Case,null=True)
+
+
+# Create your models here.
